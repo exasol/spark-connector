6812a9ddb15db33e194dc88a0a2777fa8eaa948e
Muhammet Orazov <m.orazow@gmail.com>
2018-09-14 16:58:35 +0200

    Differentiate numeric and string types in filters generation
    For example, for equality check, `str = 'abc'` and `num = 4` (not num = '4').

--- src/main/scala/com/exasol/spark/util/Filters.scala
+++ src/main/scala/com/exasol/spark/util/Filters.scala
@@ -1,6 +1,7 @@
 package com.exasol.spark.util
 
 import org.apache.spark.sql.sources._
+import org.apache.spark.sql.types._
 
 import com.typesafe.scalalogging.LazyLogging
 
@@ -19,41 +20,80 @@ object Filters extends LazyLogging {
    * @return A created Exasol where clause
    */
   @SuppressWarnings(Array("org.wartremover.warts.Option2Iterable"))
-  def createWhereClause(filters: Seq[Filter]): String =
-    filters.flatMap(filterExpr).mkString(" AND ")
+  def createWhereClause(schema: StructType, filters: Seq[Filter]): String = {
+    val dataTypes = schema.map(field => field.name -> field.dataType).toMap
+    filters.flatMap(f => filterExpr(f, dataTypes)).mkString(" AND ")
+  }
 
   /**
    * Given a Spark source [[org.apache.spark.sql.sources.Filter]], create a Exasol SQL expression.
    * Returns [[scala.None]] if an expression could not be created from the filter.
    */
-  def filterExpr(filter: Filter): Option[String] = filter match {
-    case EqualTo(a, v)            => Option(s"$a = '$v'")
-    case GreaterThan(a, v)        => Option(s"$a > $v")
-    case GreaterThanOrEqual(a, v) => Option(s"$a >= $v")
-    case LessThan(a, v)           => Option(s"$a < $v")
-    case LessThanOrEqual(a, v)    => Option(s"$a <= $v")
-    case IsNull(a)                => Option(s"($a IS NULL)")
-    case IsNotNull(a)             => Option(s"($a IS NOT NULL)")
-    case StringEndsWith(a, v)     => Option(s"($a LIKE '%$v')")
-    case StringContains(a, v)     => Option(s"($a LIKE '%$v%')")
-    case StringStartsWith(a, v)   => Option(s"($a LIKE '$v%')")
-    case In(a, vs)                => Option(s"($a IN (${vs.map(v => s"'$v'").mkString(",")}))")
-    case Not(f)                   => notExpr(f)
-    case And(lf, rf)              => binaryExpr(lf, rf, "AND")
-    case Or(lf, rf)               => binaryExpr(lf, rf, "OR")
+  def filterExpr(filter: Filter, dataTypes: Map[String, DataType]): Option[String] =
+    filter match {
+      case EqualTo(a, v)            => comparisonExpr(a, v, "=", dataTypes)
+      case Not(EqualTo(a, v))       => comparisonExpr(a, v, "!=", dataTypes)
+      case GreaterThan(a, v)        => comparisonExpr(a, v, ">", dataTypes)
+      case GreaterThanOrEqual(a, v) => comparisonExpr(a, v, ">=", dataTypes)
+      case LessThan(a, v)           => comparisonExpr(a, v, "<", dataTypes)
+      case LessThanOrEqual(a, v)    => comparisonExpr(a, v, "<=", dataTypes)
+      case IsNull(a)                => Option(s"($a IS NULL)")
+      case IsNotNull(a)             => Option(s"($a IS NOT NULL)")
+      case StringEndsWith(a, v)     => Option(s"($a LIKE '%$v')")
+      case StringContains(a, v)     => Option(s"($a LIKE '%$v%')")
+      case StringStartsWith(a, v)   => Option(s"($a LIKE '$v%')")
+      case In(a, vs)                => inExpr(a, vs, "IN", dataTypes)
+      case Not(In(a, vs))           => inExpr(a, vs, "NOT IN", dataTypes)
+      case Not(f)                   => notExpr(f, dataTypes)
+      case And(lf, rf)              => binaryExpr(lf, rf, "AND", dataTypes)
+      case Or(lf, rf)               => binaryExpr(lf, rf, "OR", dataTypes)
+      case _                        => None
+    }
+
+  def isStringValue(dataType: DataType, value: Any): String = dataType match {
+    case StringType => s"'$value'"
+    case _          => s"$value"
   }
 
-  def notExpr(filter: Filter): Option[String] =
-    filterExpr(filter).flatMap(expr => Option(s"(NOT ($expr))"))
+  def comparisonExpr(
+    name: String,
+    value: Any,
+    op: String,
+    types: Map[String, DataType]
+  ): Option[String] = types.get(name).flatMap {
+    case dtype =>
+      val newValue = isStringValue(dtype, value)
+      Option(s"$name $op $newValue")
+  }
+
+  def inExpr(
+    name: String,
+    values: Array[Any],
+    op: String,
+    types: Map[String, DataType]
+  ): Option[String] =
+    types.get(name).flatMap {
+      case dtype =>
+        val newValues = values.map(v => isStringValue(dtype, v))
+        Option(s"($name $op (${newValues.mkString(",")}))")
+    }
+
+  def notExpr(f: Filter, types: Map[String, DataType]): Option[String] =
+    filterExpr(f, types).flatMap(expr => Option(s"(NOT ($expr))"))
 
   @SuppressWarnings(Array("org.wartremover.warts.Return", "org.wartremover.warts.OptionPartial"))
-  def binaryExpr(lf: Filter, rf: Filter, op: String): Option[String] = {
-    val leftOpt = filterExpr(lf)
+  def binaryExpr(
+    lf: Filter,
+    rf: Filter,
+    op: String,
+    types: Map[String, DataType]
+  ): Option[String] = {
+    val leftOpt = filterExpr(lf, types)
     if (!leftOpt.isDefined) {
       return None // scalastyle:ignore return
     }
 
-    val rightOpt = filterExpr(rf)
+    val rightOpt = filterExpr(rf, types)
     if (!rightOpt.isDefined) {
       None
     } else {
