61d054f7737d6af87148ab3fd7f5416e11123fb5
Muhammet Orazov <m.orazow@gmail.com>
2018-09-20 14:01:04 +0200

    Gnupg files are obtained from local project/ folder for signing artifacts

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -2,7 +2,7 @@ package com.exasol.spark.sbt
 
 import sbt._
 import sbt.Keys._
-import com.typesafe.sbt.pgp.PgpKeys
+import com.typesafe.sbt.pgp.PgpKeys._
 import sbtrelease.ReleasePlugin.autoImport._
 import sbtrelease.ReleasePlugin.autoImport.ReleaseTransformations._
 import scala.xml.transform.RewriteRule
@@ -10,7 +10,7 @@ import scala.xml.transform.RuleTransformer
 
 object Publishing {
 
-  // Useful tasks to show what versions would be used if a release was performed.
+  // Useful tasks to show what versions would be used if a release was performed
   private val showReleaseVersion = taskKey[String]("Show current release version")
   private val showNextVersion = taskKey[String]("Show next release version")
 
@@ -19,8 +19,6 @@ object Publishing {
     licenses := Seq(
       "Apache License 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")
     ),
-    // autoAPIMappings := true,
-    // apiURL := Some(url("http://exasol.github.io/spark-exasol-connector")),
     publishMavenStyle := true,
     publishArtifact in Test := false,
     pomIncludeRepository := Function.const(false),
@@ -36,19 +34,18 @@ object Publishing {
     scmInfo := Some(
       ScmInfo(
         url("https://github.com/EXASOL/spark-exasol-connector"),
-        "git@github.com:EXASOL/spark-exasol-connector.git"
+        "scm:git:git@github.com:EXASOL/spark-exasol-connector.git"
       )
     ),
-    pomExtra := {
-      <developers>
-        <developer>
-          <id>morazow</id>
-          <name>Muhammet Orazov</name>
-          <url>https://github.com/morazow/</url>
-        </developer>
-      </developers>
-    },
-    // Do not include scoverage as a dependency in the pom
+    developers := List(
+      Developer(
+        id = "morazow",
+        name = "Muhammet Orazov",
+        email = "muhammet.orazov@exasol.com",
+        url = url("https://github.com/morazow/")
+      )
+    ),
+    // Do not include scoverage & wartremover plugins as a dependency in the pom
     // https://github.com/scoverage/sbt-scoverage/issues/153
     // This code was copied from https://github.com/http4s/http4s
     pomPostProcess := { (node: xml.Node) =>
@@ -63,9 +60,16 @@ object Publishing {
         }
       }).transform(node).head
     },
+    // Gnupg related settings
+    // Global scope somehow needed here; otherwise publishLocalSigned looks for these credentials
+    // in default path of ~/.sbt/gpg/
+    useGpg in Global := false,
+    pgpPublicRing in Global := baseDirectory.value / "project" / ".gnupg" / "local.pubring.asc",
+    pgpSecretRing in Global := baseDirectory.value / "project" / ".gnupg" / "local.secring.asc",
+    pgpPassphrase in Global := sys.env.get("PGP_PASSPHRASE").map(_.toArray),
     credentials ++= (for {
-      username <- Option(System.getenv().get("ARTIFACTORY_USERNAME"))
-      password <- Option(System.getenv().get("ARTIFACTORY_PASSWORD"))
+      username <- sys.env.get("ARTIFACTORY_USERNAME")
+      password <- sys.env.get("ARTIFACTORY_PASSWORD")
     } yield Credentials("Artifactory Realm", "maven.exasol.com", username, password)).toSeq,
     releaseCrossBuild := false,
     releaseCommitMessage := {
@@ -78,7 +82,7 @@ object Publishing {
     releaseTagName := s"v${version.value}",
     releaseTagComment := s"Releasing ${version.value} of module: ${name.value}",
     releaseVersionBump := sbtrelease.Version.Bump.Minor,
-    releasePublishArtifactsAction := PgpKeys.publishSigned.value,
+    releasePublishArtifactsAction := publishSigned.value,
     releaseProcess := Seq[ReleaseStep](
       checkSnapshotDependencies,
       releaseStepCommand(sbtrelease.ExtraReleaseCommands.initialVcsChecksCommand),
