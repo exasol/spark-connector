2076d32e32fb6eaeb496b78bfe33b84a3908950c
3cham <thanhtung.nov@gmail.com>
2018-10-24 12:09:28 +0200

    add configuration merging logic and tests

--- src/main/scala/com/exasol/spark/DefaultSource.scala
+++ src/main/scala/com/exasol/spark/DefaultSource.scala
@@ -20,7 +20,7 @@ class DefaultSource extends RelationProvider with DataSourceRegister with Schema
     parameters: Map[String, String]
   ): BaseRelation = {
 
-    val (queryString, manager) = fromParameters(parameters)
+    val (queryString, manager) = fromParameters(parameters, sqlContext)
     new ExasolRelation(sqlContext, queryString, None, manager)
   }
 
@@ -30,12 +30,27 @@ class DefaultSource extends RelationProvider with DataSourceRegister with Schema
     schema: StructType
   ): BaseRelation = {
 
-    val (queryString, manager) = fromParameters(parameters)
+    val (queryString, manager) = fromParameters(parameters, sqlContext)
     new ExasolRelation(sqlContext, queryString, Option(schema), manager)
   }
 
+  private[spark] def mergeConfiguration(
+    parameters: Map[String, String],
+    sparkConf: Map[String, String]
+  ): Map[String, String] = {
+    var result: Map[String, String] = parameters
+    sparkConf.keySet
+      .filter(_.contains("spark.exasol"))
+      .foreach(key => {
+        val keySuffix: String = key.substring(key.lastIndexOf(".") + 1)
+        result += (keySuffix -> sparkConf.getOrElse(key, ""))
+      })
+    result
+  }
+
   private def fromParameters(
-    parameters: Map[String, String]
+    parameters: Map[String, String],
+    sqlContext: SQLContext
   ): (String, ExasolConnectionManager) = {
     val queryString = parameters.get("query") match {
       case Some(sql) => sql
@@ -44,7 +59,7 @@ class DefaultSource extends RelationProvider with DataSourceRegister with Schema
           "A sql query string should be specified when loading from Exasol"
         )
     }
-    val config = ExasolConfiguration(parameters)
+    val config = ExasolConfiguration(mergeConfiguration(parameters, sqlContext.getAllConfs))
     val manager = ExasolConnectionManager(config)
     (queryString, manager)
   }
--- src/test/scala/com/exasol/spark/DefaultSourceSuite.scala
+++ src/test/scala/com/exasol/spark/DefaultSourceSuite.scala
@@ -2,6 +2,7 @@ package com.exasol.spark
 
 import org.apache.spark.sql.SQLContext
 
+import org.mockito.Mockito._
 import org.scalatest.{FunSuite, Matchers}
 import org.scalatest.mockito.MockitoSugar
 
@@ -16,5 +17,14 @@ class DefaultSourceSuite extends FunSuite with Matchers with MockitoSugar {
     assert(
       thrown.getMessage === "A sql query string should be specified when loading from Exasol"
     )
+  }
+
+  // scalastyle: off
+  test("mergeConfiguration should merge runtime sparkconf into user provided configuration") {
+    val sparkConf = Map[String, String]("spark.exasol.username" -> "newUsername")
+    val parameters = Map[String, String]("username" -> "oldUsername", "password" -> "password")
+
+    val newConf = new DefaultSource().mergeConfiguration(parameters, sparkConf)
+    assert(newConf.getOrElse("username", "not available") === "newUsername")
   }
 }
