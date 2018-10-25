ea884d8da8eb735b438ee622b88d96155f518c35
3cham <thanhtung.nov@gmail.com>
2018-10-25 14:01:18 +0200

    fix bug spark config isn't used when user does not specify in load options

--- src/test/scala/com/exasol/spark/DefaultSourceSuite.scala
+++ src/test/scala/com/exasol/spark/DefaultSourceSuite.scala
@@ -18,12 +18,25 @@ class DefaultSourceSuite extends FunSuite with Matchers with MockitoSugar {
     )
   }
 
-  test("mergeConfiguration should merge runtime sparkconf into user provided configuration") {
-    val sparkConf = Map[String, String]("spark.exasol.username" -> "newUsername")
+  test("mergeConfiguration should merge runtime exasol sparkConf into user provided parameters") {
+    val sparkConf = Map[String, String](
+      "spark.exasol.username" -> "newUsername",
+      "spark.exasol.host" -> "hostName",
+      "spark.other.options" -> "irrelevance"
+    )
     val parameters = Map[String, String]("username" -> "oldUsername", "password" -> "oldPassword")
 
     val newConf = new DefaultSource().mergeConfiguration(parameters, sparkConf)
+    // overwrite config if both are provided
     assert(newConf.getOrElse("username", "not available") === "newUsername")
+
+    // use config from parameters if sparkConf doesn't provide
     assert(newConf.getOrElse("password", "some random password") === "oldPassword")
+
+    // use config from sparkConf if parameters doesn't provide
+    assert(newConf.getOrElse("host", "some random host") === "hostName")
+
+    // should not contains irrelevant options for exasol
+    assert(!newConf.contains("spark.other.options") && !newConf.contains("options"))
   }
 }
