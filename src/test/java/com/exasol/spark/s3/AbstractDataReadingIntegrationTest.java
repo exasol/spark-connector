6f5107090a93bf8b2c4a4d6a87cde888316ba0ab
Muhammet Orazov <916295+morazow@users.noreply.github.com>
2023-05-22 09:59:38 +0200

    Feature/#149 proposals ck (#157)
    * proposal created during code review
    
    * proposal for AbstractDataReadingIntegrationTest
    
    ---------
    
    Co-authored-by: KK <christoph.kuhnke@exasol.com>

--- src/test/java/com/exasol/spark/s3/AbstractDataReadingIntegrationTest.java
+++ src/test/java/com/exasol/spark/s3/AbstractDataReadingIntegrationTest.java
@@ -1,18 +1,14 @@
 package com.exasol.spark.s3;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.*;
 import java.util.stream.Stream;
 
 import org.apache.spark.api.java.function.*;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.StructType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -67,55 +63,55 @@ abstract class AbstractDataReadingIntegrationTest extends S3IntegrationTestSetup
 
     @Test
     void testMapTransformation() {
+        final MapFunction<Row, Integer> doubleValue = row -> row.getInt(0) * 2;
         final Dataset<Integer> df = spark.read() //
                 .format(this.format) //
                 .option("table", table.getFullyQualifiedName()) //
                 .options(getSparkOptions()) //
                 .load() //
-                .map((MapFunction<Row, Integer>) row -> {
-                    return row.getInt(0) * 2;
-                }, Encoders.INT());
+                .map(doubleValue, Encoders.INT());
         assertThat(df.collectAsList(), contains(2, 4, 6, 8, 10, 12));
     }
 
     @Test
     void testMapPartitionsTransformation() {
+        final MapPartitionsFunction<Row, String> convertToString = it -> {
+            final List<String> result = new ArrayList<>();
+            while (it.hasNext()) {
+                result.add(String.valueOf(it.next().getInt(0)));
+            }
+            return result.iterator();
+        };
         final Dataset<String> df = spark.read() //
                 .format(this.format) //
                 .option("table", table.getFullyQualifiedName()) //
                 .options(getSparkOptions()) //
                 .load() //
-                .mapPartitions((MapPartitionsFunction<Row, String>) it -> {
-                    List<String> result = new ArrayList<>();
-                    while (it.hasNext()) {
-                        result.add(String.valueOf(it.next().getInt(0)));
-                    }
-                    return result.iterator();
-                }, Encoders.STRING());
+                .mapPartitions(convertToString, Encoders.STRING());
         assertThat(df.collectAsList(), contains("1", "2", "3", "4", "5", "6"));
     }
 
     @Test
     void testFlatMapTransformation() {
+        final FlatMapFunction<Row, Integer> repeatItem = row -> Arrays.asList(row.getInt(0), row.getInt(0)).iterator();
         final Dataset<Integer> df = spark.read() //
                 .format(this.format) //
                 .option("table", table.getFullyQualifiedName()) //
                 .options(getSparkOptions()) //
                 .load() //
-                .flatMap((FlatMapFunction<Row, Integer>) row -> {
-                    return Arrays.asList(row.getInt(0), row.getInt(0)).iterator();
-                }, Encoders.INT());
+                .flatMap(repeatItem, Encoders.INT());
         assertThat(df.collectAsList(), contains(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6));
     }
 
     @Test
     void testFilterTransformation() {
+        final FilterFunction<Row> keepEvenNumbers = row -> (row.getInt(0) % 2) == 0 ? true : false;
         final Dataset<Integer> df = spark.read() //
                 .format(this.format) //
                 .option("table", table.getFullyQualifiedName()) //
                 .options(getSparkOptions()) //
                 .load() //
-                .filter((FilterFunction<Row>) row -> (row.getInt(0) % 2) == 0 ? true : false) //
+                .filter(keepEvenNumbers) //
                 .map((MapFunction<Row, Integer>) row -> row.getInt(0), Encoders.INT());
         assertThat(df.collectAsList(), contains(2, 4, 6));
     }
