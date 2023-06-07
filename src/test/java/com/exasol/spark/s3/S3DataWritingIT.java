ccf6aa74a11a35d7104fb888711efa22a0c9d346
morazow <m.orazow@gmail.com>
2023-06-07 14:49:10 +0200

    Disable spark3.3 profile

--- src/test/java/com/exasol/spark/s3/S3DataWritingIT.java
+++ src/test/java/com/exasol/spark/s3/S3DataWritingIT.java
@@ -61,7 +61,7 @@ class S3DataWritingIT extends S3IntegrationTestSetup {
                 .option("numPartitions", "1351") //
                 .option("table", table.getFullyQualifiedName());
         final ExasolValidationException exception = assertThrows(ExasolValidationException.class, () -> df.save());
-        assertThat(exception.getMessage(), startsWith("E-SEC-19"));
+        assertThat(exception.getMessage(), startsWith("E-SEC-23"));
     }
 
     @Test
@@ -72,7 +72,7 @@ class S3DataWritingIT extends S3IntegrationTestSetup {
                 .format(this.format) //
                 .options(getSparkOptions());
         final ExasolValidationException exception = assertThrows(ExasolValidationException.class, () -> df.save());
-        assertThat(exception.getMessage(), startsWith("E-SEC-26"));
+        assertThat(exception.getMessage(), startsWith("E-SEC-19"));
     }
 
     @Test
